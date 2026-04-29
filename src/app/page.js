2f263563e7f22efa88faf6e1091f98b04a7c4977
ariekaputragani <lenna.dartati.ld@gmail.com>
2026-04-29 20:05:47 +0700

    update dikit moga

--- src/app/page.js
+++ src/app/page.js
@@ -32,37 +32,37 @@ export default function Home() {
         </div>
         <div className={styles.teks}>Mengandung 17 SDGs</div>
 
-        <div className={styles.inputGroup}>
-          <button onClick={() => {
-            actions.startNewGame();
-            router.push("/create");
-          }} className={styles.primaryButton}>Mainkan!</button>
-        </div>
-
-        <div className={styles.tombol1Button} style={{ padding: 0 }}>
-          <button onClick={() => {
-            import('sweetalert2').then((Swal) => {
-              Swal.default.fire({
-                icon: 'info',
-                title: 'Info Pengembang',
-                html: 'By Ari Eka Putragani<br><br>' +
-                  'Dosen Pembimbing:<br>' +
-                  'Dani Arifudin, M.Kom.<br>' +
-                  'Banu Dwi Putranto, M.Kom.'
-              });
-            });
-          }} className={styles.tombol1Button}><i className="fa fa-info"></i> Informasi Pengembang</button>
-        </div>
-
-        {state.age > 0 && (
-          <div className={styles.tombol2Button} style={{ padding: 0 }}>
-            <button onClick={() => {
-              actions.setupCharacter(state.profile);
-              router.push("/game");
-            }} className={styles.tombol2Button}><i className="fa fa-play"></i> Lanjutkan Hidup</button>
-          </div>
-        )}
+        <div className={styles.inputGroup} style={{ margin- top: 40px; margin-bottom: 20px}}>
+        <button onClick={() => {
+          actions.startNewGame();
+          router.push("/create");
+        }} className={styles.primaryButton}>Mainkan!</button>
       </div>
+
+      <div className={styles.tombol1Button} style={{ padding: 0 }}>
+        <button onClick={() => {
+          import('sweetalert2').then((Swal) => {
+            Swal.default.fire({
+              icon: 'info',
+              title: 'Info Pengembang',
+              html: 'By Ari Eka Putragani<br><br>' +
+                'Dosen Pembimbing:<br>' +
+                'Dani Arifudin, M.Kom.<br>' +
+                'Banu Dwi Putranto, M.Kom.'
+            });
+          });
+        }} className={styles.tombol1Button}><i className="fa fa-info"></i> Informasi Pengembang</button>
+      </div>
+
+      {state.age > 0 && (
+        <div className={styles.tombol2Button} style={{ padding: 0 }}>
+          <button onClick={() => {
+            actions.setupCharacter(state.profile);
+            router.push("/game");
+          }} className={styles.tombol2Button}><i className="fa fa-play"></i> Lanjutkan Hidup</button>
+        </div>
+      )}
     </div>
+    </div >
   );
 }
